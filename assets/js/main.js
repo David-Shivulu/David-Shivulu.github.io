15cdef93acdbdaff69dc6b44c5075da1b819b83c
David Shivulu <119101128+David-Shivulu@users.noreply.github.com>
2024-01-07 01:10:19 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,20 +30,25 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new swiper('.portfolio_container', {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        clickable: true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+/*==================== PORTFOLIO SWIPE  ====================*/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
